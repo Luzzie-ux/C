cc8ed8224af0fec3215a9559a7d24b330301ae33
Luz <estelarodrigoluz@gmail.com>
2026-05-01 19:13:47 +0100

    updated readme

--- struct.h
+++ struct.h
@@ -5,10 +5,12 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-typedef struct s_list
+typedef struct	s_list
 {
 	void		*content;
 	struct s_list	*next;
 } 			t_list;
+
+t_list	*ft_lstnew(void *content);
 
 # endif
